25a5d4bd488cd1231e93a5163045b7f1b011c34a
Sumit Singh Kushwaha <68112081+sumitsinghkushwaha@users.noreply.github.com>
2023-12-09 21:49:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // write js code here if required
+document.addEventListener("DOMContentLoaded", function () {
+  // Function to update the timer
+  function updateTimer() {
+    // Get the current date and time
+    const currentDate = new Date();
+
+    // Format the date and time
+    const formattedDate = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`;
+    const formattedTime = `${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+
+    // Display the timer in the HTML
+    document.getElementById("timer").textContent = `${formattedDate}, ${formattedTime}`;
+  }
+
+  // Update the timer every second
+  setInterval(updateTimer, 1000);
+
+  // Initial update
+  updateTimer();
+});
